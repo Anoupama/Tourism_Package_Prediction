358d9ec2ed3b8ca42d3ef86bae935c28a62e4e35
Anoupama <anoupamawaggh@gmail.com>
2026-06-14 12:12:11 +0000

    updated hosting file commit

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path=""/content/drive/MyDrive/Colab Notebooks/Project/Project: Advanced Machine Learning and MLOps: Tourism Package Prediction/tourism_project/deployment",     # the local folder containing your files
+    folder_path="tourism_project/deployment",     # the local folder containing your files
     repo_id="Anoupama/Tourism_Package_Prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
